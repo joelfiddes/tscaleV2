e2803aa81576acdaa6f96f1a98bb527e9f6eaabf
joel <joelfiddes@gmail.com>
2019-01-24 10:52:41 +0100

    autocommit

--- commit.sh
+++ commit.sh
@@ -1,8 +1,8 @@
-./update_requiremnts.sh
+./update_requirements.sh
 echo "updated requirements"
 git add -A
-echo "added to local repo"
+
 git commit -m $1
-echo "commit with message" $1
+
 git push
-echo "pushed to github"
+
