68999466efc9c2a8c1465843a306dd61038f78b9
joel <joelfiddes@gmail.com>
2019-01-24 10:55:38 +0100

    autocommit

--- commit.sh
+++ commit.sh
@@ -1,8 +1,14 @@
+#!/bin/bash
+# ./commit.sh "my commit message"
+
+if [ $# -eq 0 ]
+  then
+    echo "No commit message given"; exit
+fi
+
 ./update_requirements.sh
 echo "updated requirements"
 git add -A
-
 git commit -m $1
-
 git push
 
