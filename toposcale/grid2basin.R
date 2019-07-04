edce981a4639093389b5cce49041d1a9fd444d53
joel <joelfiddes@gmail.com>
2019-07-04 16:23:32 +0200

    rmove big file

---
+++ toposcale/grid2basin.R
@@ -0,0 +1,367 @@
+#domain should be bigger than shape
+require(raster)
+require(ncdf4)
+require(rgeos)
+args = commandArgs(trailingOnly=TRUE)
+wd=args[1]
+
+basinshp=paste0(wd,'basins/basins.shp')
+surffile=paste0(wd,"/forcing/SURF.nc")
+plevfile=paste0(wd,"/forcing/PLEV.nc")
+#surffile='SURF.nc'
+#fp=inDir+"/PLEV.nc"
+#lpfile=home + "/listpoints.txt"
+
+s = stack(surffile)
+fp = s[[1]]
+
+values(fp)<-1:ncell(s)
+shp=shapefile(basinshp)
+
+# ids of era5 grids that belong to each polygon
+basins =extract(fp,shp)
+
+
+
+#=============================================
+#	SURFACE VARIABLES
+#=============================================
+
+#read in meteo
+nc = nc_open(surffile)
+
+#=== t2m ===
+t2m_nc =ncvar_get(nc, 't2m')
+z_nc =ncvar_get(nc, 'z')
+d2m_nc =ncvar_get(nc, 'd2m')
+strd_nc =ncvar_get(nc, 'strd')
+ssrd_nc =ncvar_get(nc, 'ssrd')
+tp_nc =ncvar_get(nc, 'tp')
+tisr_nc =ncvar_get(nc, 'tisr')
+time_nc =ncvar_get(nc, 'time')
+
+#loop through basins
+t2m_mat=c()
+z_mat=c()
+d2m_mat=c()
+strd_mat=c()
+ssrd_mat=c()
+tp_mat=c()
+tisr_mat=c()
+
+for ( b in basins){
+
+# get grids that exist in basin
+print(b)
+cellIndex=na.omit(unlist(b))
+#cellIndex=b
+
+# get row/col of each grid
+myrow = ceiling(cellIndex/ncol(s))
+mycol = cellIndex-((myrow-1)*33)
+
+# extract subset of variables corresponding to those grids
+t2m_sub=t2m_nc[mycol,myrow,]
+z_sub=z_nc[mycol,myrow,]
+d2m_sub=d2m_nc[mycol,myrow,]
+strd_sub=strd_nc[mycol,myrow,]
+ssrd_sub=ssrd_nc[mycol,myrow,]
+tp_sub=tp_nc[mycol,myrow,]
+tisr_sub=tisr_nc[mycol,myrow,]
+
+if ( length(dim(t2m_sub))){
+	# the mean timeseries for basin
+	t2m_sub_mean = apply(t2m_sub, MARGIN=c(3), FUN='mean')
+	z_sub_mean = apply(z_sub, MARGIN=c(3), FUN='mean')
+	d2m_sub_mean = apply(d2m_sub, MARGIN=c(3), FUN='mean')
+	strd_sub_mean = apply(strd_sub, MARGIN=c(3), FUN='mean')
+	ssrd_sub_mean = apply(ssrd_sub, MARGIN=c(3), FUN='mean')
+	tp_sub_mean = apply(tp_sub, MARGIN=c(3), FUN='mean')
+	tisr_sub_mean = apply(tisr_sub, MARGIN=c(3), FUN='mean')
+
+	# get some stats on the distribution
+	#t2m_sub_sd = apply(t2m_sub, MARGIN=c(3), FUN='sd')
+
+	#compute p lapse rate here
+	
+	}else{
+t2m_sub_mean <- t2m_sub
+z_sub_mean <- z_sub
+d2m_sub_mean <- d2m_sub
+strd_sub_mean <- strd_sub
+ssrd_sub_mean <- ssrd_sub
+tp_sub_mean <- tp_sub
+tisr_sub_mean <- tisr_sub
+}
+
+t2m_mat=c(t2m_mat, t2m_sub_mean)
+z_mat=c(z_mat, z_sub_mean)
+d2m_mat=c(d2m_mat, d2m_sub_mean)
+strd_mat=c(strd_mat, strd_sub_mean)
+ssrd_mat=c(ssrd_mat, ssrd_sub_mean)
+tp_mat=c(tp_mat, tp_sub_mean)
+tisr_mat=c(tisr_mat, tisr_sub_mean)
+
+
+}
+
+t2m = matrix(t2m_mat, nrow=length(basins), ncol=length(t2m_sub_mean), byrow=T)
+zs = matrix(z_mat, nrow=length(basins), ncol=length(z_sub_mean), byrow=T)
+d2m = matrix(d2m_mat, nrow=length(basins), ncol=length(d2m_sub_mean), byrow=T)
+strd = matrix(strd_mat, nrow=length(basins), ncol=length(strd_sub_mean), byrow=T)
+ssrd = matrix(ssrd_mat, nrow=length(basins), ncol=length(ssrd_sub_mean), byrow=T)
+tp = matrix(tp_mat, nrow=length(basins), ncol=length(tp_sub_mean), byrow=T)
+tisr = matrix(tisr_mat, nrow=length(basins), ncol=length(tisr_sub_mean), byrow=T)
+
+# get centroids
+centroids = gCentroid(shp,byid=T)
+mylon=centroids@coords[,1]
+mylat=centroids@coords[,2]
+
+
+
+
+#=============================================
+#	writeNCDF
+#=============================================
+
+writeNcdf =function(varname,units,ncfname,title,dlname,data,time_nc,wd){
+	fillvalue <- -9999
+	basindim<- ncdim_def("basin", "degrees_north", as.double(1:length(basins)))
+	tdim <- ncdim_def("time", nc$dim$time$units, as.double(time_nc))
+	# define variables
+	tmp.def <- ncvar_def(varname, units, list(basindim, tdim), fillvalue, 
+		             dlname, prec = "single")
+	ncout <- nc_create(paste0(wd,"/forcing/",ncfname), list(tmp.def), force_v4 = TRUE)
+	# put the array
+	ncvar_put(ncout, tmp.def, data)
+	ncatt_put(ncout, "basin", "axis", "Y")
+	# add global attributes
+	ncatt_put(ncout, 0, "title", title)
+	# close the file, writing data to disk
+	nc_close(ncout)
+	}
+
+
+varname="t2m"
+units="k"
+ncfname <- "t2m.nc"
+title <- "t2m at basin centroids"
+dlname <- "2m Surface temperature"
+data=t2m
+writeNcdf(varname,units,ncfname,title,dlname,data,time_nc,wd)
+
+varname="z"
+units="m**2 s**-2"
+ncfname <- "zs.nc"
+title <- "geopotential at basin centroids"
+dlname <- "Surface geopotential"
+data=zs
+writeNcdf(varname,units,ncfname,title,dlname,data,time_nc,wd)
+
+varname="d2m"
+units="K"
+ncfname <- "d2m.nc"
+title <- "2 metre dewpoint temperature at basin centroids"
+dlname <- "2 metre dewpoint temperature"
+data=d2m
+writeNcdf(varname,units,ncfname,title,dlname,data,time_nc,wd)
+
+varname="strd"
+units="J m**-2"
+ncfname <- "strd.nc"
+title <- "Surface thermal radiation downwards at basin centroids"
+dlname <- "Surface thermal radiation downwards"
+data=strd
+writeNcdf(varname,units,ncfname,title,dlname,data,time_nc,wd)
+
+varname="ssrd"
+units="J m**-2"
+ncfname <- "ssrd.nc"
+title <- "Surface solar radiation downwards at basin centroids"
+dlname <- "Surface solar radiation downwards"
+data=ssrd
+writeNcdf(varname,units,ncfname,title,dlname,data,time_nc,wd)
+
+tp[tp<0]<-0
+tp[which(is.infinite(as.vector(tp)))]<-0 # finds and remove Inf's why these exist?! must be TP correct method
+
+varname="tp"
+units="m"
+ncfname <- "tp.nc"
+title <- "total precipitation at basin centroids"
+dlname <- "total precipitation"
+data=tp
+writeNcdf(varname,units,ncfname,title,dlname,data,time_nc,wd)
+
+varname="tisr"
+units="J m**-2"
+ncfname <- "tisr.nc"
+title <- "TOA incident solar radiation at basin centroids"
+dlname <- "TOA incident solar radiation"
+data=tisr
+writeNcdf(varname,units,ncfname,title,dlname,data,time_nc,wd)
+
+
+
+
+#=============================================
+#	PRESSURE LEVELS VARIABLES
+#=============================================
+
+#read in meteo
+nc = nc_open(plevfile)
+lev =ncvar_get(nc, 'level')
+
+#=== t2m ===
+z_nc =ncvar_get(nc, 'z')
+t_nc =ncvar_get(nc, 't')
+u_nc =ncvar_get(nc, 'u')
+v_nc =ncvar_get(nc, 'v')
+r_nc =ncvar_get(nc, 'r')
+
+
+
+#loop through basins
+z_mat=c()
+t_mat=c()
+u_mat=c()
+v_mat=c()
+r_mat=c()
+
+
+for ( b in basins){
+
+# get grids that exist in basin
+print(b)
+cellIndex=b
+
+# get row/col of each grid
+myrow = ceiling(cellIndex/ncol(s))
+mycol = cellIndex-((myrow-1)*33)
+
+# extract subset of variables corresponding to those grids
+z_sub=z_nc[mycol,myrow,,]
+t_sub=t_nc[mycol,myrow,,]
+u_sub=u_nc[mycol,myrow,,]
+v_sub=v_nc[mycol,myrow,,]
+r_sub=r_nc[mycol,myrow,,]
+
+
+if ( length(dim(z_sub))>2){
+	# the mean timeseries for basin
+	z_sub_mean = as.vector(t(apply(z_sub, MARGIN=c(3,4), FUN='mean')))
+	t_sub_mean = as.vector(t(apply(t_sub, MARGIN=c(3,4), FUN='mean')))
+	u_sub_mean = as.vector(t(apply(u_sub, MARGIN=c(3,4), FUN='mean')))
+	v_sub_mean = as.vector(t(apply(v_sub, MARGIN=c(3,4), FUN='mean')))
+	r_sub_mean = as.vector(t(apply(r_sub, MARGIN=c(3,4), FUN='mean')))
+
+
+	# get some stats on the distribution
+	#t2m_sub_sd = apply(t2m_sub, MARGIN=c(3), FUN='sd')
+
+	#compute p lapse rate here
+	
+	}else{
+z_sub_mean <- as.vector(z_sub)
+t_sub_mean <- as.vector(t_sub)
+u_sub_mean <- as.vector(u_sub)
+v_sub_mean <- as.vector(v_sub)
+r_sub_mean <- as.vector(r_sub)
+
+}
+z_mat=c(z_mat, z_sub_mean)
+t_mat=c(t_mat, t_sub_mean)
+u_mat=c(u_mat, u_sub_mean)
+v_mat=c(v_mat, v_sub_mean)
+r_mat=c(r_mat, r_sub_mean)
+
+}
+
+zp = matrix(z_mat, nrow=length(basins), ncol=length(z_sub_mean), byrow=T)
+t = matrix(t_mat, nrow=length(basins), ncol=length(t_sub_mean), byrow=T)
+u = matrix(u_mat, nrow=length(basins), ncol=length(u_sub_mean), byrow=T)
+v = matrix(v_mat, nrow=length(basins), ncol=length(v_sub_mean), byrow=T)
+r = matrix(r_mat, nrow=length(basins), ncol=length(r_sub_mean), byrow=T)
+
+# reshape to 3D
+zp_array = array(zp, dim=c(length(basins), length(time_nc), length(nlev)))
+t_array = array(t, dim=c(length(basins), length(time_nc), length(nlev)))
+u_array = array(u, dim=c(length(basins), length(time_nc), length(nlev)))
+v_array = array(v, dim=c(length(basins), length(time_nc), length(nlev)))
+r_array = array(r, dim=c(length(basins), length(time_nc), length(nlev)))
+
+# get centroids
+centroids = gCentroid(shp,byid=T)
+mylon=centroids@coords[,1]
+mylat=centroids@coords[,2]
+
+
+
+
+#=============================================
+#	writeNCDF - plevel
+#=============================================
+writeNcdf =function(varname,units,ncfname,title,dlname,data,time_nc,wd){
+	fillvalue <- -9999
+	basindim<- ncdim_def("basin", "degrees_north", as.double(1:length(basins)))
+	plevdim<- ncdim_def("levels", "mb", as.double(lev))
+	tdim <- ncdim_def("time", nc$dim$time$units, as.double(time_nc))
+	# define variables
+	tmp.def <- ncvar_def(varname, units, list(basindim, tdim, plevdim), fillvalue, 
+		             dlname, prec = "single")
+	ncout <- nc_create(paste0(wd,"/forcing/",ncfname), list(tmp.def), force_v4 = TRUE)
+	# put the array
+	ncvar_put(ncout, tmp.def, data)
+	ncatt_put(ncout, "basin", "axis", "Y")
+	ncatt_put(ncout, "plev", "axis", "Z")
+	# add global attributes
+	ncatt_put(ncout, 0, "title", title)
+	# close the file, writing data to disk
+	nc_close(ncout)
+	}
+
+
+varname="z"
+units="m**2 s**-2"
+ncfname <- "zp.nc"
+title <- "geopotential at basin centroids"
+dlname <- "geopotential"
+data=zp_array
+writeNcdf(varname,units,ncfname,title,dlname,data,time_nc,wd)
+
+varname="t"
+units="k"
+ncfname <- "t.nc"
+title <- "t at basin centroids"
+dlname <- "temperature"
+data=t_array
+writeNcdf(varname,units,ncfname,title,dlname,data,time_nc,wd)
+
+varname="u"
+units=" m s**-1"
+ncfname <- "u.nc"
+title <- "u"
+dlname <- "u"
+data=u_array
+writeNcdf(varname,units,ncfname,title,dlname,data,time_nc,wd)
+
+varname="v"
+units=" m s**-1"
+ncfname <- "v.nc"
+title <- "v"
+dlname <- "v"
+data=v_array
+writeNcdf(varname,units,ncfname,title,dlname,data,time_nc,wd)
+
+varname="r"
+units="%"
+ncfname <- "r.nc"
+title <- "Relative humidity"
+dlname <- "Relative humidity"
+data=r_array
+writeNcdf(varname,units,ncfname,title,dlname,data,time_nc,wd)
+
+
+
+
---
+++ toposcale/tscale_run_basin.py
@@ -0,0 +1,263 @@
+"""main
+
+Main toposcale run script. Requires existence of:
+	- pressurelevel data file: "PLEV.nc"
+	- surface data file: "/SURF.nc"
+	- listpoints file: "/listpoints.txt"
+
+	- runs on however many pressure levels are downloaded
+
+Object description:
+
+	* pob - pressure level object
+	* sob - surface object
+	* tob - toposcale object
+	* stat - station object (points, cluster centroids or grid centroids)
+
+Example:
+	python /home/joel/src/tscaleV2/toposcale/tscale_run.py /home/joel/sim/imis/forcing/ /home/joel/sim/imis/ /home/joel/sim/imis/forcing/ 2000-09-01 2001-09-01 FALSE
+	
+Args:
+	inDir: directory containing input meteo PLEV.nc and SURF.nc 
+		"/home/joel/sim/topomapptest/forcing/"
+	home: location of listpoints.txt
+		"/home/joel/sim/topomapptest/sim/g1m1/"
+	outDir: location where output meteo files are written
+		"/home/joel/sim/topomapptest/sim/g1m1/forcing/"
+	startTime: ISO format #"2016-08-01 18:00:00"
+	endTime: ISO format #"2016-08-01 18:00:00"
+	windCor: use sebs wind correction str: "TRUE" or "FALSE"
+Todo:
+
+"""
+
+import pandas as pd # only required to import listpoints nicely, should be able to remove
+import era5 as e5
+import tscale as ts
+import helper as hp
+import numpy as np
+import sys
+import logging
+import netCDF4 as nc
+
+#=== ARGS==============================================
+inDir=sys.argv[1] # /home/joel/sim/imis/forcing/
+home=sys.argv[2] # /home/joel/sim/imis/sim/g1
+outDir = sys.argv[3] # /home/joel/sim/imis/sim/g1/forcing
+startTime = sys.argv[4]
+endTime = sys.argv[5]
+windCor=sys.argv[6]
+basin=sys.argv[7]
+# DEBUG
+# inDir= '/home/joel/sim/imis/forcing/'
+# home='/home/joel/sim/imis/'
+# outDir = inDir 
+# startTime = '2000-09-01'
+# endTime = '2001-09-01'
+# windCor='FALSE'
+
+t2m_file=inDir+"/t2m.nc"
+d2m_file=inDir+"/d2m.nc"
+zs_file=inDir+"/zs.nc"
+ssrd_file=inDir+"/ssrd.nc"
+strd_file=inDir+"/strd.nc"
+tp_file=inDir+"/tp.nc"
+tisr_file=inDir+"/tisr.nc"
+
+zp_file=inDir+"/zp.nc"
+t_file=inDir+"/t.nc"
+u_file=inDir+"/u.nc"
+v_file=inDir+"/v.nc"
+r_file=inDir+"/r.nc"
+
+lpfile=home + "/listpoints.txt"
+# read in lispoints
+lp=pd.read_csv(lpfile)
+
+#===============================================================================
+#	Logging
+#===============================================================================
+logging.basicConfig(level=logging.DEBUG, filename=home+"/tscale_logfile", filemode="a+",
+                        format="%(asctime)-15s %(levelname)-8s %(message)s")
+
+for i in range(lp.id.size):
+
+	# station attribute structure , tz always =0 for case of ERA5
+	stat = hp.Bunch(ele = lp.ele[i], slp = lp.slp[i],asp = lp.asp[i],svf = lp.svf[i],lon = lp.lon[i], lat =lp.lat[i],sro = lp.surfRough[i],tz = lp.tz[i]  )
+
+#===============================================================================
+#	PLEVEL - mak a POB
+#===============================================================================
+	# Pressure level object 
+	zp = nc.Dataset(zp_file)
+	t = nc.Dataset(t_file)
+	u = nc.Dataset(u_file)
+	v = nc.Dataset(v_file)
+	r = nc.Dataset(r_file)
+	
+	# Datetime structure 
+	nctime = t.variables['time']
+	dtime = pd.to_datetime(nc.num2date(nctime[:],nctime.units, calendar="standard"))
+	startIndex = int(np.where(dtime==startTime)[0])#"2016-08-01 18:00:00"
+	endIndex = int(np.where(dtime==endTime)[0])#"2016-08-01 18:00:00"
+
+	# cut p.dtime to start and end
+	dtime=dtime[startIndex:endIndex]
+
+	# extract data
+	zpdat = zp.variables['z'][:, startIndex:endIndex,basin ] 
+	tdat = t.variables['t'][ :, startIndex:endIndex,basin ]
+	udat = u.variables['u'][ :, startIndex:endIndex,basin ]
+	vdat = v.variables['v'][ :,startIndex:endIndex,basin ]
+	rdat = r.variables['r'][ :,startIndex:endIndex,basin ]
+
+	# get levels
+	lev=zp.variables['levels']
+	# create pob instance/Bunch
+	pob = hp.Bunch(z=zpdat.T,t=tdat.T,u=udat.T,v=vdat.T,r=rdat.T, dtime=dtime, levels=lev)
+
+#===============================================================================
+#	Surface - Make a SOB
+#===============================================================================
+	# Pressure level object 
+	t2m = nc.Dataset(t2m_file)
+	d2m = nc.Dataset(d2m_file)
+	zs = nc.Dataset(zs_file)
+	ssrd = nc.Dataset(ssrd_file)
+	strd = nc.Dataset(strd_file)
+	tp = nc.Dataset(tp_file)
+	tisr = nc.Dataset(tisr_file)
+
+	# Datetime structure 
+	nctime = t2m.variables['time']
+	dtime = pd.to_datetime(nc.num2date(nctime[:],nctime.units, calendar="standard"))
+	startIndex = int(np.where(dtime==startTime)[0])#"2016-08-01 18:00:00"
+	endIndex = int(np.where(dtime==endTime)[0])#"2016-08-01 18:00:00"
+
+	# cut p.dtime to start and end
+	dtime=dtime[startIndex:endIndex]
+
+	# extract data
+	t2mdat = t2m.variables['t2m'][ startIndex:endIndex ,basin] 
+	d2mdat = d2m.variables['d2m'][ startIndex:endIndex ,basin]
+	zsdat = zs.variables['z'][ startIndex:endIndex ,basin]
+	ssrddat = ssrd.variables['ssrd'][ startIndex:endIndex ,basin]
+	strddat = strd.variables['strd'][ startIndex:endIndex,basin]
+	tpdat = tp.variables['tp'][ startIndex:endIndex,basin]
+	tisrdat = tisr.variables['tisr'][ startIndex:endIndex,basin]
+	
+
+	""" Convert SWin from accumulated quantities in J/m2 to 
+	instantaneous W/m2 see: 
+	https://confluence.ecmwf.int/pages/viewpage.action?pageId=104241513
+
+	Args:
+		step: timstep in seconds (era5=3600, ensemble=10800)
+
+	Note: both EDA (ensemble 3h) and HRES (1h) are accumulated over the timestep
+	and therefore treated here the same ie step = 3600s
+	https://confluence.ecmwf.int/display/CKB/ERA5+data+documentation
+				"""
+	strddatI = strddat/3600  
+	ssrddatI = ssrddat/3600
+	tisrdatI = tisrdat/3600 
+
+
+	""" convert tp from m/timestep (total accumulation over timestep) to rate in mm/h 
+
+		Args:
+			step: timstep in seconds (era5=3600, ensemble=10800)
+
+		Note: both EDA (ensemble 3h) and HRES (1h) are accumulated over the timestep
+		however in prprocessing TP is scaled to account for missing sums. Therefor tp is sum over actual timestep (1,3,6h)
+		https://confluence.ecmwf.int/display/CKB/ERA5+data+documentation
+	"""
+	# compute step in seconds for accumulated surface fields
+	a=dtime[2]-dtime[1]
+	step = a.seconds
+	tp1 = tpdat/step*60*60 # convert metres per timestep -> m/hour 
+	pmmhr = tp1	*1000 # m/hour-> mm/hour = PRATE
+	
+	""" compute surface elevation of coarse grid"""
+	gridEle = zsdat/9.80665
+
+	# create pob instance/Bunch
+	sob = hp.Bunch(t2m=t2mdat,d2m=d2mdat,z=zsdat,ssrd=ssrddatI,strd=strddatI,pmmhr=pmmhr,tp=tpdat, tisr=tisrdatI,gridEle=gridEle, dtime=dtime)
+
+
+
+
+
+
+
+	#=== toposcale object ====================================================
+
+	"""init object"""
+	t = ts.tscale(pob.z)
+
+	""" Downscale pl fields """
+	for v in ['t','u','v','r']:
+		dat = getattr(pob, v) # allows dynamic call to instance variable
+		t.tscale1D(dat,stat)
+		t.addVar(v,t.interpVar)
+	
+
+	# constrain r to interval 5-100 - do here as required by LWin parameterisation
+	t.r[t.r <5]=5
+	t.r[t.r>100]=100
+
+	tob = t
+	# compute downscaled longwave (LWf) Wm**2
+	t.lwin(sob, tob)
+
+	# compute downscaled shortwave (SWf) Wm**2m	
+	t.swin(pob, sob,tob, stat,pob.dtime)
+
+	# compute downscaled precipitation in mm/h (PRATE) and m/step (PSUM)
+	t.precip(sob,stat)
+
+	t.wind(tob)
+	t.ws[t.ws<0]=0
+
+	if windCor=="TRUE":
+		# corrected wind
+		blend = 40 # blend height
+		statblend=stat # new instance of stat for correction only 
+		statblend.ele = statblend.ele + blend # add blend to stat ele
+		v='v'
+		dat = getattr(p, v) # allows dynamic call to instance variable
+		t.tscale1D(dat,statblend)
+		t.addVar('vblend',t.interpVar)
+
+		v='u'
+		dat = getattr(p, v) # allows dynamic call to instance variable
+		t.tscale1D(dat,statblend)
+		t.addVar('ublend',t.interpVar)
+
+		# ws from vectors
+		t.wsblend = np.sqrt(tob.ublend**2+tob.vblend**2)
+
+		t.windCorRough( tob, pob, sob, stat)
+
+	 # plt.plot(t.SWfdirCor)
+	# plt.show()
+	
+
+	df = pd.DataFrame({	"TA":t.t, 
+				"RH":t.r*0.01, #meteoio 0-1
+				"VW":t.ws,
+				"DW":t.wd, 
+				"ILWR":t.LWf, 
+				"ISWR":t.SWfglob, 
+				"PINT":t.prate,
+				"PSUM":t.psum
+				},index=pob.dtime)
+	df.index.name="datetime"
+
+	# fill outstanding nan in SW routine with 0 (night)
+	df.ISWR = df.ISWR.fillna(0)
+	fileout=outDir+"/meteo"+"c"+str(i+1)+".csv"
+	df.to_csv(path_or_buf=fileout ,na_rep=-999,float_format='%.3f')
+	logging.info(fileout + " complete")
+
+
